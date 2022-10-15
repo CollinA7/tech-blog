14ef90a02ddbb97a132b554747b0f176d64f28bd
cayean <collinayala@gmail.com>
2022-10-15 14:30:26 -0500

    added initial files

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const routes = require('./controllers')
+const sequelize = require('./config/connection');
+const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// app.engine('handlebars', hbs.engine);
+// app.set ('view engine', 'handlebars');
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(require('./'));
+
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
+})
